77427a0472630f2cdec605588c4f40185ab6db25
traustid <traustid@gmail.com>
2016-07-06 23:23:07 +0200

    Added documents API call. Added support for image delivery.

--- app.js
+++ app.js
@@ -1,30 +1,57 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var _ = require('underscore');
 var elasticsearch = require('elasticsearch');
+var IMGR = require('imgr').IMGR;
 
 var app = express();
 
 var client = new elasticsearch.Client({
-	host: '127.0.0.1:9200',
-	log: 'trace'
+	host: '127.0.0.1:9200'
+//	log: 'trace'
+});
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.all('*', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+	res.header('Access-Control-Allow-Credentials', 'true')
+	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+	res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Allow-Headers')
+	next();
 });
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+	res.send('Arosenius API');
 });
 
 app.get('/documents', function(req, res) {
+	var pageSize = 30;
+
 	var query = [];
+
 	if (req.query.museum) {
 		query.push('collection.museum: "'+req.query.museum+'"');
 	}
-	if (req.query.type )
+
+	if (req.query.bundle) {
+		query.push('bundle: "'+req.query.bundle+'"');
+	}
 
 	client.search({
 		index: 'arosenius',
 		type: 'artwork',
-		size: 30,
-		from: 0,
+		size: req.query.showAll && req.query.showAll == 'true' ? 1000 : pageSize,
+		from: req.query.showAll && req.query.showAll == 'true' ? 0 : (req.query.page && req.query.page > 0 ? (req.query.page-1)*pageSize : 0),
+		sort: [
+			'bundle',
+			'page.id'
+		],
 		q: query.length > 0 ? query.join(', ') : null
 	}, function(error, response) {
 		res.json({
@@ -34,6 +61,66 @@ app.get('/documents', function(req, res) {
 				ret.id = item._id;
 				return ret;
 			})
+		});
+	});
+});
+
+app.get('/bundle/:bundle', function(req, res) {
+	var pageSize = 30;
+
+	var query = [];
+
+	query.push('bundle: "'+req.params.bundle+'"');
+
+	client.search({
+		index: 'arosenius',
+		type: 'bundle',
+		q: 'bundle: "'+req.params.bundle+'"'
+	}, function(error, response) {
+		res.json({
+			data: response.hits.hits[0]._source
+		});
+	});
+
+});
+
+app.put('/document/:id', function(req, res) {
+	console.log(req);
+	res.json({response: 'put'});
+});
+
+app.post('/document/:id', function(req, res) {
+	client.update({
+		index: 'arosenius',
+		type: 'artwork',
+		id: req.body.id,
+		body: {
+			doc: req.body
+		}
+	}, function(error, response) {
+		res.json({response: 'post'});
+	});
+});
+
+app.get('/document/:id', function(req, res) {
+	var query = [];
+	if (req.query.museum) {
+		query.push('collection.museum: "'+req.query.museum+'"');
+	}
+
+	client.search({
+		index: 'arosenius',
+		type: 'artwork',
+		size: 1,
+		from: 0,
+		q: '_id: '+req.params.id
+	}, function(error, response) {
+		res.json({
+			data: _.map(response.hits.hits, function(item) {
+				var ret = item._source;
+				ret.id = item._id;
+				return ret;
+			})[0]
 		});
 	});
 });
@@ -61,6 +148,37 @@ app.get('/museums', function(req, res) {
 		}));
 	});
 });
+
+app.get('/bundles', function(req, res) {
+	var pageSize = 30;
+
+	var query = [];
+
+	if (req.query.museum) {
+		query.push('collection.museum: "'+req.query.museum+'"');
+	}
+
+	client.search({
+		index: 'arosenius',
+		type: 'bundle',
+		size: pageSize,
+		from: req.query.page && req.query.page > 0 ? (req.query.page-1)*pageSize : 0,
+		sort: [
+			'bundle'
+		],
+		q: query.length > 0 ? query.join(', ') : null
+	}, function(error, response) {
+		res.json({
+			total: response.hits.total,
+			bundles: _.map(response.hits.hits, function(item) {
+				var ret = item._source;
+				ret.id = item._id;
+				return ret;
+			})
+		});
+	});
+});
+
 
 app.get('/technic', function(req, res) {
 	client.search({
@@ -110,6 +228,39 @@ app.get('/material', function(req, res) {
 	});
 });
 
+app.get('/types', function(req, res) {
+	client.search({
+		index: 'arosenius',
+		type: 'artwork',
+		body: {
+			"aggs": {
+				"types": {
+					"terms": {
+						"field": "type",
+						"size": 50,
+						"order": {
+							"_count": "desc"
+						}
+					}
+				}
+			}
+		}
+	}, function(error, response) {
+		res.json(_.map(response.aggregations.types.buckets, function(type) {
+			return type.key;
+		}));
+	});
+});
+
+var imgr = new IMGR({
+	cache_dir: '/tmp/imgr'
+});
+
+imgr.serve('C:\\Users\\Trausti\\Documents\\Vinna\\Arosenius\\arosenius-imagedata')
+	.namespace('/images')
+	.urlRewrite('/:path/:size/:file.:ext')
+	.using(app);
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Arosenius project API');
 });
