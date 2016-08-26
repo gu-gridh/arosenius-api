cbeca7a6fec62dcccbcd6f149fdcb8e71277bb01
traustid <traustid@gmail.com>
2016-08-26 12:13:04 +0200

    Updated response cors headers

--- app.js
+++ app.js
@@ -21,13 +21,14 @@ app.use(function(req, res, next) {
 
 	if (user === undefined || user['name'] !== 'arosenius' || user['pass'] !== 'dBe55yrPMK') {
 		res.setHeader('WWW-Authenticate', 'Basic realm="AroseniusAdminApi"');
-		res.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
-		res.header('Access-Control-Allow-Headers', 'Authorization,Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Allow-Headers')
-        res.end('Unauthorized');
-    } else {
-        next();
-    }
+	    res.header('Access-Control-Allow-Credentials', 'true')
+	    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+	    res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Allow-Headers')
+	    res.header('Access-Control-Allow-Headers', 'Authorization,Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Allow-Headers')
+		res.end('Unauthorized');
+	} else {
+		next();
+	}
 });
 
 app.use(bodyParser.urlencoded({
@@ -37,8 +38,9 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.all('*', function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Credentials', 'true')
 	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+	res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Allow-Headers')
 	res.header('Access-Control-Allow-Headers', 'Authorization,Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Allow-Headers')
 	next();
 });
