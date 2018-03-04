5e3e3b12fdfa8192e26643b5b6cc457a6c84128b
Unknown <traustid@gmail.com>
2018-03-04 18:14:53 +0100

    sorting

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ function QueryBuilder(sort, showUnpublished, showDeleted) {
 
 	// Initialize the main body of the query
 	this.queryBody = {
-//		sort: sortObject
+		sort: sortObject
 	};
 
 	if (!this.queryBody['query']) {
