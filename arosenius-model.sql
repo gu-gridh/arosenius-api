57bda6a2e62195e2f8ffdccab66620f54ae458e2
Arild Matsson <arild.matsson@gu.se>
2020-05-29 14:43:28 +0200

    Capitals in SQL, INT lengths

--- arosenius-model.sql
+++ arosenius-model.sql
@@ -12,7 +12,7 @@ DROP TABLE IF EXISTS `artwork`;
 DROP TABLE IF EXISTS `person`;
 
 CREATE TABLE `person` (
-  `id` int(3) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` INT(3) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(200) NOT NULL,
   `birth_year` VARCHAR(4),
   `death_year` VARCHAR(4),
@@ -20,64 +20,64 @@ CREATE TABLE `person` (
 );
 
 CREATE TABLE `artwork` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `insert_id` int(10),
-  `name` varchar(20),
-  `title` varchar(200),
-  `subtitle` varchar(200),
-  `deleted` boolean NOT NULL,
-  `published` boolean NOT NULL,
-  `museum_int_id` varchar(100),
-  `description` text,
-  `museum` varchar(100),
-  `archive_physloc` varchar(50),
-  `archive_title` varchar(100),
-  `museum_url` varchar(300),
-  `date_human` varchar(20),
-  `date` varchar(10), -- Using the DATE type for unspecific dates would require certain server configuration.
-  `size` varchar(200), -- JSON
-  `acquisition` varchar(200),
-  `content` text,
-  `inscription` text,
-  `material` varchar(300),
-  `creator` varchar(100),
-  `sender` int(3) UNSIGNED,
-  `recipient` int(3) UNSIGNED,
-  `literature` varchar(300),
-  `bundle` varchar(50),
+  `id` INT(6) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `insert_id` INT(6),
+  `name` VARCHAR(20),
+  `title` VARCHAR(200),
+  `subtitle` VARCHAR(200),
+  `deleted` BOOLEAN NOT NULL,
+  `published` BOOLEAN NOT NULL,
+  `museum_int_id` VARCHAR(100),
+  `description` TEXT,
+  `museum` VARCHAR(100),
+  `archive_physloc` VARCHAR(50),
+  `archive_title` VARCHAR(100),
+  `museum_url` VARCHAR(300),
+  `date_human` VARCHAR(20),
+  `date` VARCHAR(10), -- Using the DATE type for unspecific dates would require certain server configuration.
+  `size` VARCHAR(200), -- JSON
+  `acquisition` VARCHAR(200),
+  `content` TEXT,
+  `inscription` TEXT,
+  `material` VARCHAR(300),
+  `creator` VARCHAR(100),
+  `sender` INT(3) UNSIGNED,
+  `recipient` INT(3) UNSIGNED,
+  `literature` VARCHAR(300),
+  `bundle` VARCHAR(50),
   FOREIGN KEY (`sender`) REFERENCES `person` (`id`),
   FOREIGN KEY (`recipient`) REFERENCES `person` (`id`),
   UNIQUE KEY (`name`)
 );
 
 CREATE TABLE `keyword` (
-	`id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`artwork` int(10) unsigned NOT NULL,
-	`type` varchar(50) NOT NULL,
-	`name` varchar(50) NOT NULL,
+	`id` INT(7) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`artwork` INT(6) UNSIGNED NOT NULL,
+	`type` VARCHAR(50) NOT NULL,
+	`name` VARCHAR(50) NOT NULL,
   FOREIGN KEY (`artwork`) REFERENCES `artwork` (`id`),
 	UNIQUE KEY (`artwork`, `type`, `name`)
 );
 
 CREATE TABLE `image` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `artwork` int(10) unsigned NOT NULL,
-  `filename` varchar(100) NOT NULL,
-  `type` varchar(10),
-  `width` int(5) NOT NULL,
-  `height` int(5) NOT NULL,
-  `page` int(1),
-  `pageid` varchar(20),
-  `order` int(1),
-  `side` varchar(20),
+  `id` INT(6) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `artwork` INT(6) UNSIGNED NOT NULL,
+  `filename` VARCHAR(100) NOT NULL,
+  `type` VARCHAR(10),
+  `width` INT(5) NOT NULL,
+  `height` INT(5) NOT NULL,
+  `page` INT(1),
+  `pageid` VARCHAR(20),
+  `order` INT(1),
+  `side` VARCHAR(20),
   FOREIGN KEY (`artwork`) REFERENCES `artwork` (`id`)
 );
 
 CREATE TABLE `exhibition` (
-  `id` int(3) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `artwork` int(6) UNSIGNED NOT NULL,
-  `location` varchar(100) NOT NULL,
-  `year` int(4) NOT NULL,
+  `id` INT(3) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `artwork` INT(6) UNSIGNED NOT NULL,
+  `location` VARCHAR(100) NOT NULL,
+  `year` INT(4) NOT NULL,
   FOREIGN KEY (`artwork`) REFERENCES `artwork` (`id`),
   UNIQUE KEY (`artwork`, `location`, `year`)
 )
